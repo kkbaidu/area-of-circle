3d3a290cd052a7a877c95aa88a92bdbdd50df6c5
kkbaidu <102754766+kkbaidu@users.noreply.github.com>
2022-10-04 12:28:52 -0700

    Update areacircle.sol

--- areacircle.sol
+++ areacircle.sol
@@ -4,7 +4,7 @@ pragma solidity ^0.6.0;
 contract AreaOfCircle {
     function area(int128 r) public view returns (int128) {
         int128 areacircle;
-        areacircle = r*(3.142 ** 2);
+        areacircle = (r ** 2) *3.142;
         return areacircle;
     }
 }
